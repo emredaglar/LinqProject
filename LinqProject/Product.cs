455d45843f4bda6eec6106e9ad86d4782ace5c17
[Emre Dağlar] <[emre.daglar1@gmail.com]>
2024-02-22 10:17:21 +0300

    Proje dosyası ekle.

---
+++ LinqProject/Product.cs
@@ -0,0 +1,9 @@
+﻿public class Product
+{
+    public int ProductId { get; set; }
+    public int CategoryId { get; set; }
+    public string ProductName { get; set; }
+    public string ProductDescription { get; set; }
+    public decimal ProductPrice { get; set; }
+    public int UnitsInStock { get; set; }
+}
---
+++ LinqProject/Program.cs
@@ -0,0 +1,51 @@
+﻿
+using LinqProject;
+
+List<Category> categories = new List<Category>
+{
+    new Category{CategoryId=1, CategoryName="Bilgisayar"},
+    new Category{CategoryId=2, CategoryName="Telefon"}
+};
+
+List<Product> products = new List<Product>
+{
+    new Product{ProductId=1,CategoryId=1,ProductName="Acer Laptop",ProductPrice=10000,UnitsInStock=5},
+     new Product{ProductId=2,CategoryId=1,ProductName="Asus Laptop",ProductPrice=8000,UnitsInStock=3},
+    new Product{ProductId=3,CategoryId=1,ProductName="Hp Laptop",ProductPrice=6000,UnitsInStock=2},
+     new Product{ProductId=4,CategoryId=1,ProductName="Samsung Laptop",ProductPrice=5000,UnitsInStock=15},
+     new Product{ProductId=5,CategoryId=1,ProductName="Apple Laptop",ProductPrice=8000,UnitsInStock=0}
+
+};
+
+
+var result = products.Where(p => p.ProductPrice > 5000 && p.UnitsInStock > 3);
+
+foreach (var product in result)
+{
+    Console.WriteLine(product.ProductName);
+}
+
+GetProducts(products);
+static List<Product> GetProducts(List<Product> products)
+{
+    List<Product> filtredProducts = new List<Product>();
+    foreach (var product in products)
+    {
+        if (product.ProductPrice > 5000 && product.UnitsInStock > 3)
+        {
+            filtredProducts.Add(product);
+        }
+        
+
+    }
+    return filtredProducts;
+}
+GetProductsLinq(products);
+
+static List<Product> GetProductsLinq(List<Product> products)
+{
+
+    //where döngüde if de oluşturuyor.
+    return products.Where(p => p.ProductPrice > 5000 && p.UnitsInStock > 3).ToList();
+  
+}
